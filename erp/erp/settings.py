f53f43729bce8bc2fbe96a6e2edf4ddf1ed743c8
rivesae <rivesae@outlook.com>
2023-07-25 20:39:26 +0800

    STATIC_ROOT change

--- erp/erp/settings.py
+++ erp/erp/settings.py
@@ -155,7 +155,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'asset')
+STATIC_ROOT = env('STATIC_ROOT') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
